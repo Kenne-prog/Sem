8de58605e629639f5a2ce1449017925b0e5d7873
Kenne-prog <noneveragain12345@gmail.com>
2023-04-24 16:57:13 -0700

    trying rogue

--- rogue.c
+++ rogue.c
@@ -7,14 +7,37 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <time.h>
 #include "dungeon_info.h"
 #include "dungeon_settings.h"
+
 struct Dungeon* dungeon;
 
 
-// Signal handler function for SIGUSR1
+// Binary search function to pick the lock
 void signal_handler(int signal) {
-    // do nothing
+    float min = 0;
+    float max = MAX_PICK_ANGLE;
+    float mid = (min + max) / 2;
+    bool success = false;
+
+    while (!success && (max - min) > LOCK_THRESHOLD) {
+        dungeon->rogue.pick = mid;
+        usleep(TIME_BETWEEN_ROGUE_TICKS);
+
+        if (dungeon->trap.locked) {
+            if (dungeon->trap.direction == 'u') {
+                max = mid;
+            } else {
+                min = mid;
+            }
+            mid = (min + max) / 2;
+        } else if (dungeon->trap.direction == '-') {
+            success = true;
+        }
+    }
+
+    return success;
 }
 
 int main() {
@@ -36,10 +59,22 @@ int main() {
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
     sigaction(DUNGEON_SIGNAL, &sa, NULL);
+
+    // Wait for dungeon to start
     while(dungeon->running){
         pause();
     }
-    
+
+    // Pick the lock
+    srand(time(NULL)); // seed the random number generator
+    float target_value = (float)rand() / (float)RAND_MAX * MAX_PICK_ANGLE;
+    bool success = pick_lock(target_value);
+
+    if (success) {
+        printf("Rogue succeeded!\n");
+    } else {
+        printf("Rogue failed!\n");
+    }
 
     return 0;
 }
