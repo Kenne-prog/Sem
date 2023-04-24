6585fc85de55e01de2c2dd2456ca00b1df7ec144
Kenne-prog <noneveragain12345@gmail.com>
2023-04-23 17:55:52 -0700

    changed makefile

--- rogue.c
+++ rogue.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
+#include <stdbool.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+#include <signal.h>
 #include "dungeon_info.h"
 #include "dungeon_settings.h"
-
 struct Dungeon* dungeon;
 
-void barbarian_signal_handler(int signal) {
-    if (signal == DUNGEON_SIGNAL) {
-        printf("RogueIKE");
-    }
+
+// Signal handler function for SIGUSR1
+void signal_handler(int signal) {
+    // do nothing
 }
 
 int main() {
-    
-    
+    // Initialize shared memory
+    int shm_fd = shm_open(dungeon_shm_name, O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("Error creating shared memory");
+        return 1;
+    }
+    dungeon = mmap(NULL, sizeof(struct Dungeon), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (dungeon == MAP_FAILED) {
+        perror("Error mapping shared memory");
+        return 1;
+    }
+
+    // Set up signal handler
     struct sigaction sa;
-    sa.sa_handler = barbarian_signal_handler;
+    sa.sa_handler = &signal_handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
     sigaction(DUNGEON_SIGNAL, &sa, NULL);
-    
-    int fd = shm_open(dungeon_shm_name, O_RDWR, 0666);
-    dungeon = mmap(NULL, sizeof(struct Dungeon), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    
-    kill(0, DUNGEON_SIGNAL);
-
     while(dungeon->running){
         pause();
     }
-    // Send SIG_BARBARIAN_ATTACK signal to self (pid = 0)
     
 
-    
-    if (munmap(dungeon, sizeof(struct Dungeon)) == -1) {
-        perror("munmap");
-    }
-    
-    
     return 0;
 }
