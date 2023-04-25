75384ee259c688520271ea1f80a49a2634a1e290
Kenne-prog <noneveragain12345@gmail.com>
2023-04-24 18:10:13 -0700

    rogue.c testing

--- rogue.c
+++ rogue.c
@@ -24,7 +24,7 @@ void signal_handler(int signal) {
 
     while (!picked && (max - min) > LOCK_THRESHOLD) {
         dungeon->rogue.pick = mid;
-
+        usleep(TIME_BETWEEN_ROGUE_TICKS);
 
         if (dungeon->trap.locked) {
             if (dungeon->trap.direction == 'u') {
@@ -38,23 +38,14 @@ void signal_handler(int signal) {
         }
     }
 
-    return picked;
 }
 
 int main() {
 
-    int shm_fd = shm_open(dungeon_shm_name, O_RDWR, 0);
-    if (shm_fd == -1) {
-        perror("shm_open");
-        return 1;
-    }
+    int shm = shm_open(dungeon_shm_name, O_RDWR, 0);
     // Map the shared memory into the process's address space
-    dungeon = mmap(NULL, sizeof(struct Dungeon), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    if (dungeon == MAP_FAILED) {
-        perror("mmap");
-        return 1;
-    }
-
+    dungeon = mmap(NULL, sizeof(struct Dungeon), PROT_READ | PROT_WRITE, MAP_SHARED, shm_, 0);
+    
     struct sigaction sa;
     sa.sa_handler = &signal_handler;
     sigemptyset(&sa.sa_mask);
