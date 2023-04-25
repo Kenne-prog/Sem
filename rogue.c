806e09156c5b40c57dd7d21cb0b7e7c07adb267a
Kenne-prog <noneveragain12345@gmail.com>
2023-04-25 14:45:47 -0700

    testing sem

--- rogue.c
+++ rogue.c
@@ -55,7 +55,7 @@ void sem_handler(int signal) {
     for (int i = 0; i < 4; i++) {
         dungeon->treasure[i] = dungeon->spoils[i];
         printf("Rogue: Found character '%c'\n", dungeon->treasure[i]);
-        sleep(10000000000); // Wait half a second between each character
+        usleep(1000000); // Wait half a second between each character
     }
 
     // Copy the treasure to the spoils field
