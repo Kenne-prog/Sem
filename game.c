ec5c9907fb70c5ec1f15585fdb8a374cbd09b7db
Kenne-prog <noneveragain12345@gmail.com>
2023-04-25 14:57:04 -0700

    testing sem

--- game.c
+++ game.c
@@ -53,13 +53,12 @@ int main(int argc, char *argv[]) {
         execl("./rogue.o", "./rogue", NULL);
     }
     
-    // Wait for processes to complete
-
+    sem_close(door_sem_1);
+    sem_close(door_sem_2);
 
     RunDungeon(wizard_pid, rogue_pid, barbarian_pid);
 
-    sem_close(door_sem_1);
-    sem_close(door_sem_2);
+    
 
     if (munmap(dungeon, sizeof(struct Dungeon)) == -1) {
         perror("Error unmapping shared memory");
