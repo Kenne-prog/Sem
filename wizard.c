e624a1627b26f6878250732b06e526073a1a71f9
Kenne-prog <noneveragain12345@gmail.com>
2023-04-25 00:45:34 -0700

    testing wizard

--- wizard.c
+++ wizard.c
@@ -1,68 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <signal.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <ctype.h>
 #include "dungeon_info.h"
 #include "dungeon_settings.h"
+
 struct Dungeon* dungeon;
 
-char* decrypt(char message[], int key)
-{
-    int i;
-    char ch;
 
-    for(i = 0; message[i] != '\0'; i++)
-    {
-        ch = message[i];
-
-        if(isalpha(ch))
-        {
-            if(isupper(ch))
-            {
-                ch = ((ch - 'A') - key + 26) % 26 + 'A';
-            }
-            else
-            {
-                ch = ((ch - 'a') - key + 26) % 26 + 'a';
-            }
+char* caesar_cipher(char* spell, int shift) {
+    //int len = strlen(spell);
+    char* encrypted = malloc(SPELL_BUFFER_SIZE); // allocate memory for the encrypted string
+    for (int i = 0; i < SPELL_BUFFER_SIZE; i++) {
+        int ch = (int) spell[i];
+        //int og = (int) spell[i];
+        
+        if (ch >= 65 && ch <= 90) {   // uppercase letters
+            ch = ((ch - shift - 65 + 26) % 26) + 65;   // shift and wrap around
         }
-
-        message[i] = ch;
+        else if (ch >= 97 && ch <= 122) {   // lowercase letters
+            ch = ((ch - shift - 97 + 26) % 26) + 97;   // shift and wrap around
+        }
+        encrypted[i] = (char) ch;
     }
-    return message;
+    return encrypted;
 }
 
-// Signal handler function for SIGUSR1
-void signal_handler(int signal) {
-    char key = dungeon->barrier.spell[0];   // get the first character as the shift value
-    char shift = dungeon->barrier.spell[0];   // get the first character as the shift value
-    printf("Character is %c \n", key);
-    printf("SHIFFTTTTTTTTTTTTTTTTTT: %d", shift);
-    //printf("%c", key);
+void signal_handler(int sig) {
+    // set global variable to signal received
+    int shift = (int) dungeon->barrier.spell[0] % 26;   // get the first character as the shift value
     //sleep(SECONDS_TO_GUESS_BARRIER);
-    char* message = decrypt(dungeon->barrier.spell + 1, shift);
-    strcpy(dungeon->wizard.spell, message);
+    char* encrypted = caesar_cipher(dungeon->barrier.spell + 1, shift);   // pass the rest of the string to caesar_cipher
+    strcpy(dungeon->wizard.spell, encrypted);   // copy the encrypted string back to the struct
+    free(encrypted);   // free the memory allocated for the encrypted string
 }
 
 int main() {
-    // Initialize shared memory
-    int shm_fd = shm_open(dungeon_shm_name, O_RDWR, 0666);
+    // setup signal handler using sigaction
+
+    // open shared memory object
+    int shm_fd = shm_open(dungeon_shm_name, O_RDWR, 0);
     if (shm_fd == -1) {
-        perror("Error creating shared memory");
-        return 1;
-    }
-    dungeon = mmap(NULL, sizeof(struct Dungeon), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    if (dungeon == MAP_FAILED) {
-        perror("Error mapping shared memory");
-        return 1;
+        perror("shm_open");
+        exit(EXIT_FAILURE);
     }
 
-    // Set up signal handler
+    // map shared memory object to process address space
+    dungeon = mmap(NULL, sizeof(struct Dungeon), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (dungeon == MAP_FAILED) {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
     struct sigaction sa;
     sa.sa_handler = &signal_handler;
     sigemptyset(&sa.sa_mask);
@@ -71,7 +63,11 @@ int main() {
     while(dungeon->running){
         pause();
     }
-    
 
+
+
+    // unmap shared memory object from process address space
+    munmap(dungeon, sizeof(struct Dungeon));
+    shm_unlink(dungeon_shm_name);
     return 0;
 }
