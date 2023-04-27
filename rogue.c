d01caf57ec85f003cb6db780f7761a26de56f369
Kenne-prog <noneveragain12345@gmail.com>
2023-04-27 14:59:15 -0700

    semaphores

--- rogue.c
+++ rogue.c
@@ -65,8 +65,7 @@ void signal_handler(int signal) {
     printf("Rogue: Copied treasure to spoils field: %s\n", dungeon->spoils);
 
     // Release the semaphores
-    sem_post(door_sem_1);
-    sem_post(door_sem_2);
+
 
     }
 }
