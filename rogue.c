0f3b22bad3d56b3bdf1d32a4af673847d27e0359
Kenne-prog <noneveragain12345@gmail.com>
2023-04-27 13:55:14 -0700

    semaphores

--- rogue.c
+++ rogue.c
@@ -59,7 +59,7 @@ void signal_handler(int signal) {
     dungeon->treasure[4] = '\0';
     dungeon->spoils[4] = '\0';
     // Copy the treasure to the spoils field
-    strcpy(dungeon->spoils, dungeon->treasure);
+    //strcpy(dungeon->spoils, dungeon->treasure);
     printf("Rogue: Copied treasure to spoils field: %s\n", dungeon->spoils);
 
     // Release the semaphores
