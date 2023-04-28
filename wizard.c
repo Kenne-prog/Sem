d64d933aac83f7814d6c611ea199de2b58623326
Kenne-prog <noneveragain12345@gmail.com>
2023-04-27 23:02:43 -0700

    semaphores

--- wizard.c
+++ wizard.c
@@ -47,6 +47,9 @@ void signal_handler(int signal) {
 
         printf("Wizard: Entering the treasure room\n");
         sem_post(door_sem_1);
+        if (strlen(dungeon->spoils) == 4){
+            sem_t *door_sem_1 = sem_open("/LeverOne", 0);
+        }
         printf("Wiz: holding\n");
     }
     
