a3ffac09933590a1ce648cd62445604ed6b4f7a6
Kenne-prog <noneveragain12345@gmail.com>
2023-04-28 13:42:51 -0700

    semaphores

--- rogue.c
+++ rogue.c
@@ -50,7 +50,7 @@ void signal_handler(int signal) {
     sem_wait(door_sem_2);
 
     // Get the treasure from the dungeon
-    dungeon->spoils[0] = dungeon->treasure[0];
+    dungeon->spoils = "";
 
     printf("Rogue: has already '%s'\n", dungeon->spoils);
 
