70c65fac4d7b2ac8a943f1e664ebea9f87718ff6
Kenne-prog <noneveragain12345@gmail.com>
2023-04-28 13:53:18 -0700

    semaphores

--- rogue.c
+++ rogue.c
@@ -42,7 +42,7 @@ void signal_handler(int signal) {
     }
     else if (signal == SEMAPHORE_SIGNAL){
     sem_t *door_sem_1 = sem_open("/LeverOne", 0);
-    sem_t *door_sem_2 = sem_open("/LeverTwo", 0);
+    sem_t *door_sem_2 = sem_open("/LeverTo", 0);
 
 
     // Wait for both semaphores to be available
