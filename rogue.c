ab387346978bb4acbe8e3f26995f858810e30f20
Kenne-prog <noneveragain12345@gmail.com>
2023-04-27 22:26:43 -0700

    semaphores

--- rogue.c
+++ rogue.c
@@ -26,7 +26,7 @@ void signal_handler(int signal) {
 
         while (!picked && (max - min) > LOCK_THRESHOLD) {
             dungeon->rogue.pick = mid;
-            usleep(15000);
+            usleep(20000);
 
             if (dungeon->trap.locked) {
             if (dungeon->trap.direction == 'u') {
