8ae858ab5fe8774961728c2d58e7cf68e8c897dd
Kenne-prog <noneveragain12345@gmail.com>
2023-04-28 13:13:50 -0700

    semaphores

--- rogue.c
+++ rogue.c
@@ -63,7 +63,6 @@ void signal_handler(int signal) {
         dungeon->spoils[1] = dungeon->treasure[1];
         dungeon->spoils[2] = dungeon->treasure[2];
         dungeon->spoils[3] = dungeon->treasure[3]; 
-        usleep(10000);
     }
     
     
